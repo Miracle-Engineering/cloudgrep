5e03effd5ab3fa824f54add41896a3a464ab32c5
Remy DeWolf <RemyDeWolf@users.noreply.github.com>
2022-05-04 13:11:07 -0700

    revert: test lint using exp/slices

--- pkg/util/slices.go
+++ pkg/util/slices.go
@@ -1,5 +1,7 @@
 package util
 
+//TODO use exp/slices in the future but as of now lint fails with this error
+// no export data for \"golang.org/x/exp/slices
 func Contains(sl []string, name string) bool {
 	for _, value := range sl {
 		if value == name {
