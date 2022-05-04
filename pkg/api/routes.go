52b8e2f1aa05cd9b220a5d3637ad4bdcda76811f
Remy DeWolf <RemyDeWolf@users.noreply.github.com>
2022-05-04 14:34:39 -0700

    do not use global var for datastore in api

--- pkg/api/routes.go
+++ pkg/api/routes.go
@@ -9,22 +9,24 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/run-x/cloudgrep/pkg/config"
+	"github.com/run-x/cloudgrep/pkg/datastore"
 )
 
-func SetupMiddlewares(group *gin.RouterGroup, cfg config.Config) {
+func SetupMiddlewares(group *gin.RouterGroup, cfg config.Config, ds datastore.Datastore) {
 	if cfg.Logging.IsDev() {
 		group.Use(logAllQueryParams(cfg), logAllRequests(cfg))
 	}
+	group.Use(setDatastore(ds))
 }
 
-func SetupRoutes(router *gin.Engine, cfg config.Config) {
+func SetupRoutes(router *gin.Engine, cfg config.Config, ds datastore.Datastore) {
 	root := router.Group(cfg.Web.Prefix)
 
 	root.GET("/", gin.WrapH(GetHome(cfg.Web.Prefix)))
 	root.GET("/static/*path", gin.WrapH(GetAssets(cfg.Web.Prefix)))
 
 	api := root.Group("/api")
-	SetupMiddlewares(api, cfg)
+	SetupMiddlewares(api, cfg, ds)
 
 	api.GET("/info", GetInfo)
 	api.GET("/resources", GetResources)
