dd4ccd6454a903b00a1c8b772605b55a88f23b3d
Remy DeWolf <RemyDeWolf@users.noreply.github.com>
2022-05-08 14:06:08 -0700

    do not join tables, simplify sql queries

--- pkg/datastore/sqlitestore.go
+++ pkg/datastore/sqlitestore.go
@@ -17,8 +17,10 @@ import (
 
 type SQLiteStore struct {
 	logger *zap.Logger
-	db     *CloudGrepDB
+	db     *gorm.DB
 }
+
+type resourceId string
 
 func NewSQLiteStore(ctx context.Context, cfg config.Config) (*SQLiteStore, error) {
 	s := SQLiteStore{}
@@ -39,12 +41,12 @@ func NewSQLiteStore(ctx context.Context, cfg config.Config) (*SQLiteStore, error
 	)
 	s.logger = cfg.Logging.Logger
 	//create the DB client
-	db, err := gorm.Open(sqlite.Open(cfg.Datastore.DataSourceName),
+	var err error
+	s.db, err = gorm.Open(sqlite.Open(cfg.Datastore.DataSourceName),
 		&gorm.Config{Logger: gormLogger})
 	if err != nil {
 		return nil, fmt.Errorf("can't create the SQLite database: %w", err)
 	}
-	s.db = &CloudGrepDB{db}
 
 	// Migrate the schema
 	if err = s.db.AutoMigrate(&model.Resource{}, &model.Property{}, &model.Tag{}); err != nil {
@@ -54,76 +56,75 @@ func NewSQLiteStore(ctx context.Context, cfg config.Config) (*SQLiteStore, error
 	return &s, nil
 }
 
-type CloudGrepDB struct {
-	*gorm.DB
-}
-
-//joinOnTags joins on tags table using resource_id as key
-func (db *CloudGrepDB) joinOnTags() *CloudGrepDB {
-	return &CloudGrepDB{db.Joins("JOIN tags on tags.resource_id=resources.id")}
-}
-
-//filter build the query to only return filtered results
-func (db *CloudGrepDB) filter(filter model.Filter) *CloudGrepDB {
-	if filter.IsEmpty() {
-		return db
-	}
-	//working a session allow chaining using OR on where statement
-	// without session: (=tag1) OR ((=tag1) AND (=tag2))
-	// with session: (=tag1) OR (=tag2)
-	table := db.joinOnTags().Session(&gorm.Session{})
-
-	// use a group condition to keep all the matching tags
-	// where (=tag1) OR (=tag2)
-	// https://gorm.io/docs/advanced_query.html#Group-Conditions
-	result := table
-	for _, tag := range filter.Tags {
-		if tag.Value == "*" {
-			//wild card - check for any value
-			result = result.
-				Or(
-					table.Where("tags.key=? and tags.value like ?", tag.Key, "%"),
-				)
-		} else {
-			//exact value
-			result = result.
-				Or(
-					table.Where("tags.key=? and tags.value=?", tag.Key, tag.Value),
-				)
+func (s *SQLiteStore) getResourceIds(ctx context.Context, filter model.Filter) ([]resourceId, error) {
+	var resourceIds []resourceId
+	db := s.db.Model(&model.Tag{}).Select("resource_id")
+	if !filter.IsEmpty() {
+		for _, tag := range filter.Tags {
+			if tag.Value == "*" {
+				//wild card - check for any value
+				db = db.Or("key=? and value like ?", tag.Key, "%")
+			} else {
+				//exact value
+				db = db.Or("key=? and value=?", tag.Key, tag.Value)
+			}
 		}
+		// our data model has one row per tag, we need a group by to only keep the results matching all tags
+		/* table tags
+		//original data, tag filter = team=infra and cluster=staging
+		1. i-123	cluster		staging
+		2. i-123	team		infra
+		3. i-124	cluster		staging
+		4. i-124	team		dev
+
+		//where team=infra OR cluster=staging
+		1. i-123	cluster		staging
+		2. i-123	team		infra
+		3. i-124	cluster		staging
+
+		// group by + count on id
+		1. i-123	2 <- we only want to keep this row
+		2. i-124	1
+		*/
+		db = db.Group("resource_id").
+			Having("count(resource_id)=?", filter.Tags.DistinctKeys())
 	}
-	// our data model has one row per tag, we need a group by to only keep the results matching all tags
-	/* table resource join tag
-	1. i-123	us-east-1	cluster		staging
-	2. i-123	us-east-1	team		infra
-	3. i-124	us-east-1	cluster		staging
-	3. i-124	us-east-1	team		dev
 
-	when searching on team=infra and cluster=staging, we should only return i-123
-	# group by + count on id
-	1. i-123	2 <- we only want to keep this row
-	2. i-124	1
+	result := db.Find(&resourceIds)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return resourceIds, nil
+}
 
-	*/
-	result = result.Group("resources.id").
-		Having("count(resources.id)=?", filter.Tags.DistinctKeys())
+func (s *SQLiteStore) getResourcesById(ctx context.Context, ids []resourceId) ([]*model.Resource, error) {
+	var resources []*model.Resource
+	if len(ids) == 0 {
+		return resources, nil
+	}
+	result := s.db.Preload("Tags").Preload("Properties").Find(&resources, ids)
 
-	return &CloudGrepDB{result}
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return resources, nil
+
 }
 
 func (s *SQLiteStore) GetResources(ctx context.Context, filter model.Filter) ([]*model.Resource, error) {
 	var resources []*model.Resource
 
-	result := s.db.filter(filter).
-		Preload("Tags").Preload("Properties").
-		Find(&resources)
-
-	if result.Error != nil {
-		return nil, fmt.Errorf("can't get resources from database: %w", result.Error)
+	ids, err := s.getResourceIds(ctx, filter)
+	if err != nil {
+		return nil, fmt.Errorf("can't get resources from database: %w", err)
+	}
+	resources, err = s.getResourcesById(ctx, ids)
+	if err != nil {
+		return nil, fmt.Errorf("can't get resources from database: %w", err)
 	}
 	s.logger.Sugar().Infow("Getting resources: ",
 		zap.Object("filter", filter),
-		zap.Int("count", int(result.RowsAffected)),
+		zap.Int("count", len(resources)),
 	)
 	return resources, nil
 }
