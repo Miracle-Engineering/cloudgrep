111837365b9a887588d0efc179f06ed932088f66
Enes Ljevakovic <eneslj@gmail.com>
2022-05-10 17:22:16 +0300

    auto align code

--- fe/src/utils/amplitude/amplitude.ts
+++ fe/src/utils/amplitude/amplitude.ts
@@ -1,22 +1,22 @@
 import amplitude from 'amplitude-js';
 
 export const initAmplitude = () => {
-    amplitude.getInstance().init(process.env.REACT_APP_AMPLITUDE_API_KEY || '');
-    amplitude.getInstance().logEvent('TEST EVENT');
+	amplitude.getInstance().init(process.env.REACT_APP_AMPLITUDE_API_KEY || '');
+	amplitude.getInstance().logEvent('TEST EVENT');
 };
 
 export const setAmplitudeUserDevice = (installationToken: string) => {
-    amplitude.getInstance().setDeviceId(installationToken);
+	amplitude.getInstance().setDeviceId(installationToken);
 };
 
 export const setAmplitudeUserId = (userId: string | null) => {
-    amplitude.getInstance().setUserId(userId);
+	amplitude.getInstance().setUserId(userId);
 };
 
 export const setAmplitudeUserProperties = (properties: any) => {
-    amplitude.getInstance().setUserProperties(properties);
+	amplitude.getInstance().setUserProperties(properties);
 };
 
 export const sendAmplitudeData = (eventType: string, eventProperties: any) => {
-    amplitude.getInstance().logEvent(eventType, eventProperties);
+	amplitude.getInstance().logEvent(eventType, eventProperties);
 };
