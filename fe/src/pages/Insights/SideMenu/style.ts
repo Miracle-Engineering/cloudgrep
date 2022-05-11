591b1015c06a05e22fef7cdd62f8deb7f2593af8
Enes Ljevakovic <eneslj@gmail.com>
2022-05-11 19:51:15 +0300

    auto aligned code

--- fe/src/pages/Insights/SideMenu/style.ts
+++ fe/src/pages/Insights/SideMenu/style.ts
@@ -1,13 +1,13 @@
 import { CSSProperties } from 'react';
 
 export const sideMenuStyle: CSSProperties = {
-    height: '100%',
-    maxHeight: '100%',
-    overflow: 'scroll',
-    width: '50%',
-    border: '1px solid black',
-    position: 'absolute',
-    right: '0px',
-    top: '0px',
-    backgroundColor: 'white',
+	height: '100%',
+	maxHeight: '100%',
+	overflow: 'scroll',
+	width: '50%',
+	border: '1px solid black',
+	position: 'absolute',
+	right: '0px',
+	top: '0px',
+	backgroundColor: 'white',
 };
