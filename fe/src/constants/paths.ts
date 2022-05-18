d33aaa917c630c0532e676540746aa4a68127444
Enes Ljevakovic <eneslj@gmail.com>
2022-05-18 17:52:35 +0200

    auto aligned code

--- fe/src/constants/paths.ts
+++ fe/src/constants/paths.ts
@@ -5,4 +5,5 @@ export const GET_RESOURCES_PATH = 'resources';
 
 export const getTagsPath = (): string => `${process.env.REACT_APP_MOCK_API_URL}${GET_TAGS_PATH}`;
 export const getResourcesPath = (): string => `${process.env.REACT_APP_API_URL}${GET_RESOURCES_PATH}`;
-export const getFilteredResourcesPath = (data: MockTag): string => `${process.env.REACT_APP_API_URL}${GET_RESOURCES_PATH}?tag[${data.Key}]=${data.Value}`;
+export const getFilteredResourcesPath = (data: MockTag): string =>
+	`${process.env.REACT_APP_API_URL}${GET_RESOURCES_PATH}?tag[${data.Key}]=${data.Value}`;
