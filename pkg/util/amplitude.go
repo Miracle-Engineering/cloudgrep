ae56f40830449c8866a016e4277b951cb8667371
nsarupr <sarupriyanilesh@outlook.com>
2022-05-23 17:15:02 +0530

    Resolve test cyclic dependency

--- pkg/util/amplitude.go
+++ pkg/util/amplitude.go
@@ -2,12 +2,14 @@ package util
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
 
+	"github.com/run-x/cloudgrep/pkg/config"
 	"github.com/run-x/cloudgrep/pkg/version"
 
 	"github.com/google/uuid"
@@ -15,7 +17,7 @@ import (
 	"github.com/tcnksm/go-gitconfig"
 )
 
-func GetGitConfigEmail() string {
+func GetConfigEmail() string {
 	email, err := gitconfig.Email()
 	if err != nil {
 		return "no_user"
@@ -28,17 +30,21 @@ func SessionId() int64 {
 }
 
 const (
-	AMPLITUDE_TOKEN = "751db5fc75ff34f08a83381f4d54ead6"
-	BASE_EVENT      = "BASE_EVENT"
-	AMPLITUDE_URL   = "https://api2.amplitude.com/2/httpapi"
+	amplitudeToken = "751db5fc75ff34f08a83381f4d54ead6"
+	amplitudeUrl   = "https://api2.amplitude.com/2/httpapi"
+	application    = "cloudgrep"
 )
 
-var SESSION_ID = SessionId()
-var USER_ID = GetGitConfigEmail()
-var VALID_EVENTS = []string{BASE_EVENT}
+const (
+	BaseEvent = "BASE_EVENT"
+)
+
+var sessionId = SessionId()
+var userId = GetConfigEmail()
+var validEvents = []string{BaseEvent}
 
 func isValidEvent(eventType string) bool {
-	for _, validEvent := range VALID_EVENTS {
+	for _, validEvent := range validEvents {
 		if validEvent == eventType {
 			return true
 		}
@@ -46,59 +52,80 @@ func isValidEvent(eventType string) bool {
 	return false
 }
 
-func sendAmplitudeEvent(eventType string, eventProperties map[string]interface{}, userProperties map[string]interface{}) (int, error) {
-	if version.IsDev() {
-		return 0, nil
-	}
-
-	if !isValidEvent(eventType) {
-		return 0, fmt.Errorf("Invalid Event Type: %s", eventType)
-	}
-
+func GenerateAmplitudeEvent(eventType string, eventProperties map[string]interface{}) (map[string]interface{}, error) {
 	if eventProperties == nil {
 		eventProperties = make(map[string]interface{})
 	}
 
-	if userProperties == nil {
-		userProperties = make(map[string]interface{})
-	}
+	eventProperties["application"] = application
 
-	systemInfo, _ := goInfo.GetInfo()
+	systemInfo, err := goInfo.GetInfo()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get system info: %w", err)
+	}
 
 	id := uuid.New().String()
 	event := map[string]interface{}{
-		"user_id":          USER_ID,
+		"user_id":          userId,
 		"device_id":        systemInfo.Hostname,
 		"event_type":       eventType,
 		"event_properties": eventProperties,
-		"user_properties":  userProperties,
 		"app_version":      version.Version,
 		"platform":         systemInfo.Platform,
 		"insert_id":        id,
-		"session_id":       SESSION_ID,
+		"session_id":       sessionId,
 	}
 
-	amplitudeBody, _ := json.Marshal(map[string]interface{}{"api_key": AMPLITUDE_TOKEN, "events": []interface{}{event}})
+	return event, nil
+}
+
+func SendAmplitudeEvent(ctx context.Context, cfg config.Config, eventType string, eventProperties map[string]interface{}) (int, error) {
+	if version.IsDev() {
+		return 1, fmt.Errorf("dev application, not sending events to amplitude") //dev application, not sending events to amplitude
+	}
+
+	if !isValidEvent(eventType) {
+		return 1, fmt.Errorf("invalid event type: %s, not sending events to amplitude\n", eventType) // not sending invalid events
+	}
+
+	amplitudeEvent, err := GenerateAmplitudeEvent(eventType, eventProperties)
+	if err != nil {
+		return 1, fmt.Errorf("failed to generate amplitude event: %w", err) // don't send event to amplitude
+	}
+
+	amplitudeBody, err := json.Marshal(map[string]interface{}{"api_key": amplitudeToken, "events": []interface{}{amplitudeEvent}})
+	if err != nil {
+		return 1, fmt.Errorf("failed to marshal amplitude event: %w", err) // don't send event to amplitude
+	}
 
 	client := &http.Client{Timeout: time.Second * 10}
-	req, _ := http.NewRequest("POST", AMPLITUDE_URL, bytes.NewBuffer(amplitudeBody))
+	req, err := http.NewRequest("POST", amplitudeUrl, bytes.NewBuffer(amplitudeBody))
+	if err != nil {
+		return 1, fmt.Errorf("failed to create amplitude request: %w", err) // don't send event to amplitude
+	}
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Accept", "*/*")
 	response, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		defer response.Body.Close()
-		body, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(response.Status)
-		fmt.Println(body)
-		fmt.Print("lalala")
-		fmt.Println(string(body))
+		return 1, fmt.Errorf("failed amplitude response: %w", err) //failed to get amplitude response
 	}
 
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		responseBody, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return 1, fmt.Errorf("failed to read amplitude response body: %w", err)
+		}
+		cfg.Logging.Logger.Sugar().Debug("amplitude response status code: %d", response.StatusCode)
+		cfg.Logging.Logger.Sugar().Debug("amplitude response body: %s", string(responseBody))
+	}
 	return 0, nil
 }
 
-func SendEvent(eventType string, eventProperties map[string]interface{}, userProperties map[string]interface{}) {
-	go sendAmplitudeEvent(eventType, eventProperties, userProperties)
+func SendEvent(ctx context.Context, cfg config.Config, eventType string, eventProperties map[string]interface{}) {
+	go func() {
+		_, err := SendAmplitudeEvent(ctx, cfg, eventType, eventProperties)
+		if err != nil {
+			cfg.Logging.Logger.Sugar().Debug(err)
+		}
+	}()
 }
