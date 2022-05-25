735e5a7b0626c2df27693941e6ce669969060604
Patrick Fiedler <patrick@runx.dev>
2022-05-25 12:32:58 -0500

    Add empty example test

--- pkg/provider/aws/aws_provider_test.go
+++ pkg/provider/aws/aws_provider_test.go
@@ -1,5 +1,13 @@
 package aws
 
-func emptyProvider() AWSProvider {
+import (
+	"testing"
 
+	"github.com/run-x/cloudgrep/mocks/pkg/provider/aws"
+)
+
+func TestEC2(t *testing.T) {
+	provider := AWSProvider{}
+	mock := aws.NewEC2Client(t)
+	provider.ec2Client = mock
 }
