692b2f8f3bb62a6b122763b96a659d58bcd600ef
Ankur Dahiya <ankur@runx.dev>
2022-06-07 09:31:48 -0700

    undo static del

---
+++ static/ga.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+(function () {
+	window.ga =
+		window.ga ||
+		function () {
+			(ga.q = ga.q || []).push(arguments);
+		};
+
+	ga.l = +new Date();
+
+	ga('create', 'UA-228613342', 'auto');
+
+	ga('send', 'pageview');
+})();
