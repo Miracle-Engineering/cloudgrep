8256b423520d60c448ed711089b4cd702e2d5ae9
Juan Diego Palomino <jdpcbs@gmail.com>
2022-06-09 13:43:06 -0700

    Adding elegant stack traces only on verbose run (#85)
    * Adding elegant stack traces only on verbose run
    
    * Update pkg/util/errors.go
    
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>
    
    * Update pkg/util/errors.go
    
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>
    
    * Update pkg/util/errors.go
    
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>
    
    * test fixing
    
    Co-authored-by: Patrick Fiedler <rabbitfang@users.noreply.github.com>

---
+++ pkg/util/errors.go
@@ -0,0 +1,57 @@
+package util
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"runtime/debug"
+)
+
+var showErrorStackTrace = false
+
+func EnableErrorStackTrace() {
+	showErrorStackTrace = true
+}
+
+type stackTraceError struct {
+	StackTrace string
+	Err        error
+}
+
+func (e stackTraceError) Error() string {
+	return e.Err.Error()
+}
+func (e stackTraceError) Unwrap() error { return e.Err }
+
+func GetStackTrace(e error) string {
+	var cgerr stackTraceError
+	if errors.As(e, &cgerr) {
+		return cgerr.StackTrace
+	}
+	return ""
+}
+
+func AddStackTrace(e error) error {
+	if showErrorStackTrace {
+		var cgerr stackTraceError
+		if errors.As(e, &cgerr) {
+			return e
+		}
+		return stackTraceError{
+			StackTrace: string(debug.Stack()),
+			Err:        e,
+		}
+	}
+	return e
+}
+
+func PrintStackTrace(e error, w io.Writer) {
+	var err error
+	stackTrace := GetStackTrace(e)
+	if stackTrace != "" {
+		_, err = fmt.Fprintf(w, "Stack Trace:\n%v\n%v", stackTrace, e)
+	}
+	if err != nil {
+		panic(err)
+	}
+}
