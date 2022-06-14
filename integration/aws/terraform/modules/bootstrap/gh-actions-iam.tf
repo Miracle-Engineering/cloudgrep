48cdff36dbe70ed9ea65bd228507d026cd516330
Patrick Fiedler <patrick@runx.dev>
2022-06-14 17:49:13 -0500

    Test terraform change

--- integration/aws/terraform/modules/bootstrap/gh-actions-iam.tf
+++ integration/aws/terraform/modules/bootstrap/gh-actions-iam.tf
@@ -4,6 +4,8 @@ locals {
   gh_actions_oidc_config     = jsondecode(data.http.github_actions_oidc_config.body)
 }
 
+
+
 data "http" "github_actions_oidc_config" {
   url = local.gh_actions_oidc_config_url
 }
