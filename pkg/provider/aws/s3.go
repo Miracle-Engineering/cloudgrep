90703261cccfb4045fe715912c4f0e2e30dc0490
JD <jdpcbs@gmail.com>
2022-06-14 22:59:52 -0700

    Fixing the S3 bucket issue of fetching tags
    Root of the cause: S3 buckets are "half" global. That means when you list them, you don't mention region, you just list all in your account. When you want to get metadata like tags however,  you need to pass in the region the bucket was "created" on, otherwise it fails. Here's what's changing:
    
    1. Buckets are now considered global resources for opta, as they should be as the list api call is global
    2. The s3 fetch is no longer manually generated, but manually written as we need to go through extra steps to get the bucket's region before sending the requests for the tags.

--- pkg/provider/aws/s3.go
+++ pkg/provider/aws/s3.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/aws/smithy-go"
@@ -17,7 +16,7 @@ func (p *Provider) register_s3(mapping map[string]mapper) {
 	mapping["s3.Bucket"] = mapper{
 		FetchFunc: p.fetch_s3_Bucket,
 		IdField:   "Name",
-		IsGlobal:  false,
+		IsGlobal:  true,
 	}
 }
 
@@ -38,8 +37,21 @@ func (p *Provider) fetch_s3_Bucket(ctx context.Context, output chan<- model.Reso
 }
 func (p *Provider) getTags_s3_Bucket(ctx context.Context, resource types.Bucket) (model.Tags, error) {
 	client := s3.NewFromConfig(p.config)
-	input := &s3.GetBucketTaggingInput{}
+	locationInput := &s3.GetBucketLocationInput{Bucket: resource.Name}
+	locationOutput, err := client.GetBucketLocation(ctx, locationInput)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get %s location: %w", "s3.Bucket", err)
+	}
+	tempConfig := p.config.Copy()
+	switch location := locationOutput.LocationConstraint; location {
+	case "":
+		tempConfig.Region = "us-east-1"
+	default:
+		tempConfig.Region = string(location)
+	}
+	client = s3.NewFromConfig(tempConfig)
 
+	input := &s3.GetBucketTaggingInput{}
 	input.Bucket = resource.Name
 
 	output, err := client.GetBucketTagging(ctx, input)
