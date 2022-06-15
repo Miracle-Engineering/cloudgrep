d98558a7ae525dbe790611e6ab5052fd0ebe45e8
JD <jdpcbs@gmail.com>
2022-06-15 10:02:59 -0700

    wip

--- pkg/provider/aws/zz_register.go
+++ pkg/provider/aws/zz_register.go
@@ -1,11 +1,8 @@
 package aws
 
-import ()
-
 func (p *Provider) registerGeneratedTypes(mapping map[string]mapper) {
 	p.register_ec2(mapping)
 	p.register_elb(mapping)
 	p.register_lambda(mapping)
 	p.register_rds(mapping)
-	p.register_s3(mapping)
 }
