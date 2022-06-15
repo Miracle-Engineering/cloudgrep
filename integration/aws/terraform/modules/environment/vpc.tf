4bb59b447052356516f56146de1015dc8d8e9b8f
Patrick Fiedler <patrick@runx.dev>
2022-06-15 17:27:50 -0500

    Move integration test resources into non-default VPC

---
+++ integration/aws/terraform/modules/environment/vpc.tf
@@ -0,0 +1,5 @@
+module "vpc" {
+  source = "../vpc"
+
+  id = "main"
+}
---
+++ integration/aws/terraform/modules/vpc/subnets.tf
@@ -0,0 +1,26 @@
+locals {
+  subnet_az_letters = ["a", "b", "c"]
+  subnet_cidrs      = module.subnet_addrs.network_cidr_blocks
+  subnet_azs        = [for s in local.subnet_az_letters : "${data.aws_region.current.name}${s}"]
+  subnet_az_map     = { for s in local.subnet_az_letters : s => "${data.aws_region.current.name}${s}" }
+}
+
+resource "aws_subnet" "private" {
+  for_each = local.subnet_az_map
+
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = each.value
+  cidr_block              = local.subnet_cidrs[each.key]
+  map_public_ip_on_launch = false
+
+  tags = {
+    "test" : "vpc-${var.id}-subnet-${each.key}"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = local.subnet_az_map
+
+  subnet_id      = aws_subnet.private[each.key].id
+  route_table_id = aws_route_table.private.id
+}
