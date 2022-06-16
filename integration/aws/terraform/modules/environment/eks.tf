9aa7c999e9030bf9ad6b19dea1a37f17b55f5f61
JD <jdpcbs@gmail.com>
2022-06-16 13:03:08 -0700

    wip

--- integration/aws/terraform/modules/environment/eks.tf
+++ integration/aws/terraform/modules/environment/eks.tf
@@ -1,7 +1,7 @@
 module "eks" {
   source             = "../eks"
   id                 = "main"
-  vpc_id             = module.vpc.vpc_id
+  vpc_id             = module.vpc.id
   private_subnet_ids = module.vpc.private_subnet_ids
   cluster_role       = "eks-cluster-default-role"
   node_group_role    = "eks-cluster-node-group-role"
