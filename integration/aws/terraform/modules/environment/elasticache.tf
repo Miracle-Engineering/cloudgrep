b12f46a953cae2b1321183bcccc8b83bfb84c110
JD <jdpcbs@gmail.com>
2022-06-16 23:26:43 -0700

    generated redis code

--- integration/aws/terraform/modules/environment/elasticache.tf
+++ integration/aws/terraform/modules/environment/elasticache.tf
@@ -37,18 +37,18 @@ resource "aws_elasticache_subnet_group" "main" {
 
 resource "aws_elasticache_replication_group" "redis_cluster" {
   count                         = local.redis_cluster_count
-  automatic_failover_enabled    = true
+  automatic_failover_enabled    = false
   auto_minor_version_upgrade    = true
   security_group_ids            = [aws_security_group.elasticache.id]
   subnet_group_name             = aws_elasticache_subnet_group.main.id
   replication_group_id          = "test-${count.index}-${random_string.redis_name_hash.result}"
   replication_group_description = "Elasticache test test-${count.index}-${random_string.redis_name_hash.result}"
-  node_type                     = "cache.m4.large"
+  node_type                     = "cache.t3.small"
   engine_version                = "6.x"
-  number_cache_clusters         = 2
+  number_cache_clusters         = 1
   port                          = 6379
   apply_immediately             = true
-  multi_az_enabled              = true
+  multi_az_enabled              = false
   auth_token                    = random_password.redis_auth.result
   transit_encryption_enabled    = true
   at_rest_encryption_enabled    = true
@@ -56,5 +56,8 @@ resource "aws_elasticache_replication_group" "redis_cluster" {
   snapshot_retention_limit      = 0
   lifecycle {
     ignore_changes = [engine_version, replication_group_id, replication_group_description]
+  }
+  tags = {
+    test = "elasticache-cluster-${count.index}"
   }
 }
