155512a420e9c979cd1650a760d211672bd7cec4
nsarupr <sarupriyanilesh@outlook.com>
2022-06-20 17:56:29 +0530

    Update sns.tf

--- integration/aws/terraform/modules/environment/sns.tf
+++ integration/aws/terraform/modules/environment/sns.tf
@@ -1,18 +1,11 @@
-locals {
-  sns_count = 2
-}
-
 resource "random_string" "sns_topic_name" {
-  count = local.sns_count
-
   length  = 8
   special = false
   upper   = false
 }
 
 resource "aws_sns_topic" "topic" {
-  count           = local.sns_count
-  name            = "testing--${count.index}-${random_string.sns_topic_name[count.index].result}"
+  name            = "testing-0-${random_string.sns_topic_name.result}"
   delivery_policy = <<EOF
 {
   "http": {
@@ -36,22 +29,20 @@ EOF
     ignore_changes = [name, name_prefix]
   }
   tags = {
-    test : "sns-topic-${count.index}"
+    test : "sns-topic-0"
     IntegrationTest : "true"
   }
 }
 
 ## SNS topic policy
 resource "aws_sns_topic_policy" "default" {
-  count = local.sns_count
-  arn   = aws_sns_topic.topic[count.index].arn
+  arn = aws_sns_topic.topic.arn
 
   policy = data.aws_iam_policy_document.sns_topic_policy.json
 }
 
 data "aws_iam_policy_document" "sns_topic_policy" {
-  count     = local.sns_count
-  policy_id = "__default_policy_ID-${count.index}"
+  policy_id = "__default_policy_ID"
 
   statement {
     actions = [
@@ -74,9 +65,9 @@ data "aws_iam_policy_document" "sns_topic_policy" {
     }
 
     resources = [
-      aws_sns_topic.topic[count.index].arn,
+      aws_sns_topic.topic.arn,
     ]
 
-    sid = "__default_statement_ID-${count.index}"
+    sid = "__default_statement_ID"
   }
 }
