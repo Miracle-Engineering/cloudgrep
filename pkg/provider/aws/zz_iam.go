102983bb61cda48bb6f1e0c43488e52cd8e123ed
JD <jdpcbs@gmail.com>
2022-06-24 10:35:43 -0700

    wip

--- pkg/provider/aws/zz_iam.go
+++ pkg/provider/aws/zz_iam.go
@@ -12,12 +12,6 @@ import (
 )
 
 func (p *Provider) register_iam(mapping map[string]mapper) {
-	mapping["iam.InstanceProfile"] = mapper{
-		ServiceEndpointID: "iam",
-		FetchFunc:         p.fetch_iam_InstanceProfile,
-		IdField:           "InstanceProfileId",
-		IsGlobal:          true,
-	}
 	mapping["iam.OpenIDConnectProvider"] = mapper{
 		ServiceEndpointID: "iam",
 		FetchFunc:         p.fetch_iam_OpenIDConnectProvider,
@@ -30,22 +24,10 @@ func (p *Provider) register_iam(mapping map[string]mapper) {
 		IdField:           "Arn",
 		IsGlobal:          true,
 	}
-	mapping["iam.Role"] = mapper{
-		ServiceEndpointID: "iam",
-		FetchFunc:         p.fetch_iam_Role,
-		IdField:           "RoleId",
-		IsGlobal:          true,
-	}
 	mapping["iam.SAMLProvider"] = mapper{
 		ServiceEndpointID: "iam",
 		FetchFunc:         p.fetch_iam_SAMLProvider,
 		IdField:           "Arn",
-		IsGlobal:          true,
-	}
-	mapping["iam.User"] = mapper{
-		ServiceEndpointID: "iam",
-		FetchFunc:         p.fetch_iam_User,
-		IdField:           "UserId",
 		IsGlobal:          true,
 	}
 	mapping["iam.VirtualMFADevice"] = mapper{
@@ -54,50 +36,6 @@ func (p *Provider) register_iam(mapping map[string]mapper) {
 		IdField:           "SerialNumber",
 		IsGlobal:          true,
 	}
-}
-
-func (p *Provider) fetch_iam_InstanceProfile(ctx context.Context, output chan<- model.Resource) error {
-	client := iam.NewFromConfig(p.config)
-	input := &iam.ListInstanceProfilesInput{}
-
-	resourceConverter := p.converterFor("iam.InstanceProfile")
-	paginator := iam.NewListInstanceProfilesPaginator(client, input)
-	for paginator.HasMorePages() {
-		page, err := paginator.NextPage(ctx)
-
-		if err != nil {
-			return fmt.Errorf("failed to fetch %s: %w", "iam.InstanceProfile", err)
-		}
-
-		if err := resourceconverter.SendAllConvertedTags(ctx, output, resourceConverter, page.InstanceProfiles, p.getTags_iam_InstanceProfile); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-func (p *Provider) getTags_iam_InstanceProfile(ctx context.Context, resource types.InstanceProfile) (model.Tags, error) {
-	client := iam.NewFromConfig(p.config)
-	input := &iam.ListInstanceProfileTagsInput{}
-
-	input.InstanceProfileName = resource.InstanceProfileName
-
-	output, err := client.ListInstanceProfileTags(ctx, input)
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch %s tags: %w", "iam.InstanceProfile", err)
-	}
-	tagField_0 := output.Tags
-
-	var tags model.Tags
-
-	for _, field := range tagField_0 {
-		tags = append(tags, model.Tag{
-			Key:   *field.Key,
-			Value: *field.Value,
-		})
-	}
-
-	return tags, nil
 }
 
 func (p *Provider) fetch_iam_OpenIDConnectProvider(ctx context.Context, output chan<- model.Resource) error {
@@ -184,50 +122,6 @@ func (p *Provider) getTags_iam_Policy(ctx context.Context, resource types.Policy
 	return tags, nil
 }
 
-func (p *Provider) fetch_iam_Role(ctx context.Context, output chan<- model.Resource) error {
-	client := iam.NewFromConfig(p.config)
-	input := &iam.ListRolesInput{}
-
-	resourceConverter := p.converterFor("iam.Role")
-	paginator := iam.NewListRolesPaginator(client, input)
-	for paginator.HasMorePages() {
-		page, err := paginator.NextPage(ctx)
-
-		if err != nil {
-			return fmt.Errorf("failed to fetch %s: %w", "iam.Role", err)
-		}
-
-		if err := resourceconverter.SendAllConvertedTags(ctx, output, resourceConverter, page.Roles, p.getTags_iam_Role); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-func (p *Provider) getTags_iam_Role(ctx context.Context, resource types.Role) (model.Tags, error) {
-	client := iam.NewFromConfig(p.config)
-	input := &iam.ListRoleTagsInput{}
-
-	input.RoleName = resource.RoleName
-
-	output, err := client.ListRoleTags(ctx, input)
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch %s tags: %w", "iam.Role", err)
-	}
-	tagField_0 := output.Tags
-
-	var tags model.Tags
-
-	for _, field := range tagField_0 {
-		tags = append(tags, model.Tag{
-			Key:   *field.Key,
-			Value: *field.Value,
-		})
-	}
-
-	return tags, nil
-}
-
 func (p *Provider) fetch_iam_SAMLProvider(ctx context.Context, output chan<- model.Resource) error {
 	client := iam.NewFromConfig(p.config)
 	input := &iam.ListSAMLProvidersInput{}
@@ -252,50 +146,6 @@ func (p *Provider) getTags_iam_SAMLProvider(ctx context.Context, resource types.
 	output, err := client.ListSAMLProviderTags(ctx, input)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch %s tags: %w", "iam.SAMLProvider", err)
-	}
-	tagField_0 := output.Tags
-
-	var tags model.Tags
-
-	for _, field := range tagField_0 {
-		tags = append(tags, model.Tag{
-			Key:   *field.Key,
-			Value: *field.Value,
-		})
-	}
-
-	return tags, nil
-}
-
-func (p *Provider) fetch_iam_User(ctx context.Context, output chan<- model.Resource) error {
-	client := iam.NewFromConfig(p.config)
-	input := &iam.ListUsersInput{}
-
-	resourceConverter := p.converterFor("iam.User")
-	paginator := iam.NewListUsersPaginator(client, input)
-	for paginator.HasMorePages() {
-		page, err := paginator.NextPage(ctx)
-
-		if err != nil {
-			return fmt.Errorf("failed to fetch %s: %w", "iam.User", err)
-		}
-
-		if err := resourceconverter.SendAllConvertedTags(ctx, output, resourceConverter, page.Users, p.getTags_iam_User); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-func (p *Provider) getTags_iam_User(ctx context.Context, resource types.User) (model.Tags, error) {
-	client := iam.NewFromConfig(p.config)
-	input := &iam.ListUserTagsInput{}
-
-	input.UserName = resource.UserName
-
-	output, err := client.ListUserTags(ctx, input)
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch %s tags: %w", "iam.User", err)
 	}
 	tagField_0 := output.Tags
 
