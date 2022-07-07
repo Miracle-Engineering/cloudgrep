9313b4266c583f7f3e4034b9f52c7e62baf3b55e
Enes Ljevakovic <eneslj@gmail.com>
2022-07-07 08:46:30 +0200

    auto format code

--- fe/src/pages/Insights/InsightFilter/style.ts
+++ fe/src/pages/Insights/InsightFilter/style.ts
@@ -1,13 +1,13 @@
 import { HOVER_BLUE } from 'constants/colors';
 
 export const filterHeader = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '100%',
+	display: 'flex',
+	justifyContent: 'space-between',
+	width: '100%',
 };
 
 export const filterIcon = {
-    '&:hover': {
-        color: HOVER_BLUE,
-    },
+	'&:hover': {
+		color: HOVER_BLUE,
+	},
 };
