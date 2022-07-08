fc53ebc919ce66d2dffada221edbdd5b1e8061ca
Enes Ljevakovic <eneslj@gmail.com>
2022-07-08 19:14:36 +0200

    Group prefix added for fitler tags

--- fe/src/components/AccordionFilter/types.ts
+++ fe/src/components/AccordionFilter/types.ts
@@ -1,18 +1,24 @@
-import { Field, ValueType } from 'models/Field';
-import { Tag } from 'models/Tag';
+import { Field, FieldGroup, ValueType } from 'models/Field';
 
 export interface AccordionFilterProps {
 	id: string;
 	label: string;
 	field: Field;
+	fieldGroup: FieldGroup;
 	hasSearch: boolean;
 }
 export interface AccordionItemProps {
 	field: Field;
-	handleChange: (event: React.ChangeEvent<HTMLInputElement>, field: Field, item: ValueType) => void;
+	handleChange: (
+		event: React.ChangeEvent<HTMLInputElement>,
+		fieldGroup: FieldGroup,
+		field: Field,
+		item: ValueType
+	) => void;
 	isChecked: boolean;
 	item: ValueType;
 	handleOnly: (item: ValueType) => void;
 	handleAll: () => void;
 	singleItem: string;
+	fieldGroup: FieldGroup;
 }
